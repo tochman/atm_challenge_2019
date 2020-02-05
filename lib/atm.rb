53f119f6097efaa0861b9c4e3de6997348f2b1dd
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-05 12:10:22 +0100

    Fix formatting for atm and spec file

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,9 +1,11 @@
 class Atm
     attr_accessor :funds
     require 'date' 
+    
     def initialize
         @funds = 1000
-      end
+    end
+
     def withdraw(amount, pin_code, account)
         case
         when insufficient_funds_in_account?(amount, account)
@@ -20,24 +22,31 @@ class Atm
             perform_transaction(amount, account)
         end
     end
+
     private
+
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
     end
+
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
     end
+
     def insufficient_funds_in_atm?(amount)
         @funds < amount
     end
+
     def perform_transaction(amount, account)
         @funds -= amount
         account.balance = account.balance - amount
         { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
     end
+
     def card_expired?(exp_date)
         Date.strptime(exp_date, '%m/%y') < Date.today
     end
+    
     def account_disabled?(account_status)
         account_status != :active
     end
