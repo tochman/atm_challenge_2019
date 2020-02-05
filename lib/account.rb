2371ad60e8f5c2d20f4e0a550a1cfd818f9ba1fb
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-05 16:06:55 +0100

    Initializes attributes for account

--- lib/account.rb
+++ lib/account.rb
@@ -2,23 +2,40 @@ class Account
     STANDARD_VALIDITY_YRS = 5
 end
 
-    random_pincode = rand(1000..9999)
+random_pincode = rand(1000..9999)
+
+    def initialize(attrs = {})
+        @account_status = :active
+        set_owner(attrs[:owner])
+    end
 
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
     end
 
-    
-
-    def withdraw(amount, pin code, account)
-        case 
-        when
-        when
-        else
-        end
+    def self.deactivate(account)
+        account.account_status = :deactivated
     end
 
     private
+
+    def set_owner(obj)
+        obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner
+        raise "An Account owner is required"
+    end
+    
+
+
+    #def withdraw(amount, pin code, account)
+      #  case 
+      #  when
+      #  when
+      #  else
+       # end
+    #end
 
     
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,17 @@
 require './lib/account.rb'
     describe Account do
+        let(:person) {instance_double('Person', name: 'Thomas')}
+        subject { described_class.new({owner: person})}
+
+        it 'is expected to have an owner' do
+            expect(subject.owner).to eq person
+        end
+
+        it 'is expected to raise error if no owner is set' do
+            expect {described_class.new }.to raise_error 'An Account owner is required'
+        end
         
+
         # before do 
          #   allow(account_owner).to receive(:balance).and_return(1000)
          #   allow(account_owner).to receive(:balance=)
@@ -17,14 +28,19 @@ require './lib/account.rb'
         expect(number_length).to eq 4
         end
 
-
-
         it 'is expected to have an expiry date on initialize' do
             expected_date = Date.today.next_year(5).strftime("%m/%y")
             expect(subject.exp_date).to eq expected_date
         end
         
+        it 'is expected to have :active status on initialize' do
+            expect(subject.account_status).to eq :active
+        end
 
+        it 'deactivates account using the class method' do
+            Account.deactivate(subject)
+            expect(subject.account_status).to eq :deactivated
+        end
     
     end
 
