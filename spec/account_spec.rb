7eff7b88461ca2bbb79b3c91f45bb18e7a96ed53
lobson999 <lobson999@protonmail.com>
2020-02-05 15:19:07 +0100

    expiry date

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,10 @@
 require './lib/account.rb'
     describe Account do
-        let(:account_owner) { instance_double('Account', pin_code: '1234', account_status: :active)}
-        before do 
-            allow(account).to receive(:balance).and_return(1000)
-            allow(account).to receive(:balance=)
-        end
+        
+        # before do 
+         #   allow(account_owner).to receive(:balance).and_return(1000)
+         #   allow(account_owner).to receive(:balance=)
+        #end
 
         it 'has 1000$ on initialize' do
             expect(subject.funds).to eq 1000
@@ -16,6 +16,14 @@ require './lib/account.rb'
         number_length = Math.log10(number).to_i + 1
         expect(number_length).to eq 4
         end
+
+
+
+        it 'is expected to have an expiry date on initialize' do
+            expected_date = Date.today.next_year(5).strftime("%m/%y")
+            expect(subject.exp_date).to eq expected_date
+        end
+        
 
     
     end
