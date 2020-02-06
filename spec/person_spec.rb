7fba68424ca2fcd7092c78a597432bfae366a8cd
lobson999 <lobson999@protonmail.com>
2020-02-06 16:29:47 +0100

    integrating all parts

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -42,6 +42,31 @@ describe Person do
         it 'can deposit funds' do
             expect(subject.deposit(100)).to be_truthy
         end
+
+        it 'funds are added to the account balance - deducted from cash' do
+            subject.cash = 100
+            subject.deposit(100)
+            expect(subject.account.balance)to be 100
+            expect(subject.cash).to be 0
+        end
+
+        it 'can withdraw funds' do
+            command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+            expect(command.call).to be_truthy
+        end
+
+        it 'withdraw is expected to raise an error if no ATM is passed in' do
+            command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account) }
+            expect { command.call }.to raise_error 'An ATM is required'
+        end
+
+        it 'funds are added to cash - deducted from account balance' do
+            subject.cash = 100
+            subject.deposit(100)
+            subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm)
+            expect(subject.account.balance).to be 0
+            expect(subject.cash).to be 100
+        end
     end
 
 
@@ -51,6 +76,7 @@ describe Person do
             expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
         end
     end
+
 end   
 
 
